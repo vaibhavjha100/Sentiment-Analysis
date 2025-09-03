191300ed626eee9cc9ba6d26975db37ad03b063e
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-09-03 17:47:13 +1000

    update __init__.py to expose finsenti_pipeline for sentiment analysis

--- finsenti/__init__.py
+++ finsenti/__init__.py
@@ -4,7 +4,7 @@ Init file for the finsenti package.
 
 __version__ = "0.1.0"
 
-from .preprocessing import preprocessing_pipeline
+from .pipeline import finsenti_pipeline
 
-__all__ = ["preprocessing_pipeline"]
+__all__ = ["finsenti_pipeline"]
 
